82c3d3decab56e7fab8a1fed4edee6e7587dfd4a
liutc <17681102655@163.com>
2023-04-01 02:42:28 +0800

    nacos源码打通

--- nacos-1.4.1/example/src/main/java/com/alibaba/nacos/example/App.java
+++ nacos-1.4.1/example/src/main/java/com/alibaba/nacos/example/App.java
@@ -30,8 +30,8 @@ import com.alibaba.nacos.api.naming.NamingService;
 public class App {
     public static void main(String[] args) throws NacosException {
         Properties properties = new Properties();
-        properties.setProperty("serverAddr", "21.34.53.5:8848,21.34.53.6:8848");
-        properties.setProperty("namespace", "quickStart");
+        properties.setProperty("serverAddr", "localhost:8848");
+        properties.setProperty("namespace", "public");
         NamingService naming = NamingFactory.createNamingService(properties);
         naming.registerInstance("nacos.test.3", "11.11.11.11", 8888, "TEST1");
         naming.registerInstance("nacos.test.3", "2.2.2.2", 9999, "DEFAULT");
